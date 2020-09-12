adec25865718e381c6a8d84270d9c7a7c1709105
navarrowebapi <navarro.fabio@gmail.com>
2020-09-12 16:17:50 -0300

    rotas e CRUD

--- routes/product-route.js
+++ routes/product-route.js
@@ -21,7 +21,7 @@ router.post('/', function (req, res){
     });
 });
 
-//Get=> localhost:3000/api/produtos
+//GetAll=> localhost:3000/api/produtos
 router.get('/', function(req, res){
     Produto.find(function(err, prods){
         if(err)
@@ -31,6 +31,72 @@ router.get('/', function(req, res){
             message: "retorno ok de todos os produtos",
             allProducts: prods
         });
+    });
+});
+
+//GetById=> localhost:3000/api/produtos/ID
+router.get('/:productId', function(req, res){
+    const id = req.params.productId;
+    Produto.findById(id, function(err, produto){
+        if (err){
+            res.status(500).json({
+                message: "Erro ao tentar encontrar produto; ID mal formado"
+            });
+        }else if(produto == null){
+            res.status(400).json({
+                message: "produto não encontrado para o id passado"
+            });
+        }else{
+            res.status(200).json({
+                message: "produto encontrado",
+                produto: produto
+            });
+        }
+    });
+});
+
+
+//Put=> localhost:3000/api/produtos/ID
+router.put('/:productId', function(req, res){
+    const id = req.params.productId;
+    console.log(id)
+    Produto.findById(id, function(err, produto){
+        if(err){
+            res.status(500).json({
+                message:"Erro ao tentar encontrar produto; Id mal formado"
+            });
+        }else if(produto == null){
+            res.status(400).json({
+                message: "produto não encontrado para o Id passado"
+            });
+        }else{
+            produto.nome = req.body.nome;
+            produto.preco = req.body.preco;
+            produto.descricao = req.body.descricao;
+
+            produto.save(function(error){
+                if(error)
+                    res.send("Erro ao tentar atualizar o produto", error);
+                
+                    res.status(200).json({
+                        message: "produto atualizado com sucesso"
+                    });
+            });
+        }
+    });
+});
+
+//Delete=> localhost:3000/api/produtos/ID
+router.delete('/:productId', function(req, res){
+    Produto.findByIdAndRemove(req.params.productId, (err, produto) => {
+        if(err) 
+            res.status(500).send("Erro ao deletar ", err)
+
+        const response ={
+            message: "Produto removido com sucesso",
+            id: produto.id
+        };
+        return res.status(200).send(response);
     });
 });
 
