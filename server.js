24f9a2aee191e84c692d2c072a730baa9bc7ee95
navarrowebapi <navarro.fabio@gmail.com>
2020-09-12 14:56:07 -0300

    rotas Create e Read

--- server.js
+++ server.js
@@ -2,28 +2,30 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 //Configurar o app para usar o body-parser e transformar as req em JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+//Persistência
+const connectionString = "mongodb+srv://posgraduacao123:posgraduacao123@cluster0.jquuj.mongodb.net/bdpos?retryWrites=true&w=majority";
+mongoose.connect(connectionString,  {useNewUrlParser:true, useUnifiedTopology: true, useFindAndModify: false});
 
 //Definir porta onde o server vai responder
 const port = process.env.PORT || 3000;
 
 //Definindo as Rotas
 const router = express.Router();//intercepta todas as rotas
-
-//Middleware
-router.use(function(req, res, next){
-    console.log("Interceptação pelo Middleware ok"); //LOG, Validações, Autenticações
-    next();
-});
-
-router.get('/', (req, res) => res.send("rota teste ok"));
+const productRoute = require('./routes/product-route');
+const indexRoute = require('./routes/index-route');
 
 //Vincular a aplicação (app) com o motor de rotas 
 // '/api' é o caminho padrão para as APIs REST
-app.use('/api', router);
+//rota principal
+app.use('/api', indexRoute);
+//rota para produto
+app.use('/api/produtos/', productRoute);
 
 app.listen(port, () => {
     console.log("server is up and running...on port ", port);
